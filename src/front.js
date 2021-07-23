2838127d00eb4689b2c962aa91f4b79bac5faca1
asmir <asmir.belkic@gmail.com>
2021-07-23 19:24:54 +0200

    update fast

--- src/front.js
+++ src/front.js
@@ -27,39 +27,8 @@ function toggleAll(source) {
 	}
 }
 
-// function initTabNav() {
-// 	const tabMenu = document.querySelectorAll(".sidebar-app .nav-item");
-// 	const tabContent = document.querySelectorAll(".content section");
-// 	// var newTitle = document.querySelector("body > div.container > aside > div > a.nav-item.active").dataset.title
-// 	// document.getElementById('page-title').innerText = newTitle
-// 	if (tabMenu.length && tabContent.length) {
-// 		tabMenu[0].classList.add("active");
-// 		tabContent[0].classList.add("active");
-
-// 		function activeTab(index) {
-// 			tabContent.forEach(function (section) {
-// 				section.classList.remove("active");
-// 			});
-// 			tabContent[index].classList.add("active");
-
-// 			tabMenu.forEach(function (menu) {
-// 				menu.classList.remove("active");
-// 			});
-// 			tabMenu[index].classList.add("active");
-// 		}
-
-// 		tabMenu.forEach(function (itemMenu, index) {
-// 			itemMenu.addEventListener("click", function () {
-// 				activeTab(index);
-// 				// var newTitle = itemMenu.dataset.title
-// 				// document.getElementById('page-title').innerText = newTitle
-// 			});
-// 		});
-// 	}
-// }
-// initTabNav();
 $(".content .tab-pannel").first().show();
-function openTab(evt, tabName) {
+function openTab(event, tabName) {
 	// Declare all variables
 	var i, tabcontent, tablinks;
 
@@ -74,10 +43,9 @@ function openTab(evt, tabName) {
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace("active", "");
 	}
-
 	// Show the current tab, and add an "active" class to the button that opened the tab
 	$("#" + tabName).fadeIn(120);
-	$(evt.currentTarget).addClass("active");
+	$(event.currentTarget).addClass("active");
 }
 
 $(".nav-item").click(function () {
--- src/ytpl2mp3.js
+++ src/ytpl2mp3.js
@@ -4,8 +4,18 @@ const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const { remote } = require("electron");
 const $ = require("jquery");
+
+// Toastr notifications
+const toastr = require("toastr");
+toastr.options = {
+	debug: false,
+	positionClass: "toast-bottom-left",
+	onclick: null,
+};
+
+const cp = require("child_process");
+
 const appDir = remote.app.getAppPath();
-const itunesAPI = require("node-itunes-search");
 var pathToFfmpeg = require("ffmpeg-static").replace(
 	"app.asar",
 	"app.asar.unpacked"
@@ -36,6 +46,7 @@ window.onload = () => {
 			chemin = setting.path || process.env.HOME || process.env.USERPROFILE;
 			messageBox = setting.messageBox;
 			debug = setting.debug;
+			format = setting.format;
 			document.getElementById("path_text").value = chemin;
 			if (debug) {
 				document.getElementById("checkDebug").checked = true;
@@ -67,13 +78,13 @@ window.onload = () => {
 
 function download() {
 	startTime = Date.now();
-	let link = document.getElementById("playlist-id").value;
-	format = document.querySelector('input[name="radio"]:checked').id;
-
+	let link = $("#playlist-id").val();
 	ytpl
 		.getPlaylistID(link)
 		.then((playlistID) => {
-			ytpl(playlistID, { limit: Infinity })
+			ytpl(playlistID, {
+				limit: Infinity,
+			})
 				.then((playlist) => {
 					document.getElementById("text-out").innerHTML =
 						"Téléchargement de la playlist : " + playlist.title + "<br>";
@@ -83,7 +94,7 @@ function download() {
 						"<br><br>";
 					if (!fs.existsSync(chemin + "/" + playlist.title))
 						fs.mkdirSync(chemin + "/" + playlist.title);
-					dl_track_from_playlist(playlist, 0);
+					// dl_track_from_playlist(playlist, 0);
 				})
 				.catch((err) => {
 					return afficher_err(err.name, err.message);
@@ -93,72 +104,43 @@ function download() {
 			try {
 				if (ytdl.getVideoID(link)) return download_track(link);
 			} catch (error) {
-				return afficher_err(
-					"Wrong link",
-					"Aucun fichier disponible au téléchargement"
-				);
+				console.error(error);
 			}
 		});
 }
-
+// Cette fonction récupère le dernier element d'un tableau .last()
+if (!Array.prototype.last) {
+	Array.prototype.last = function () {
+		return this[this.length - 1];
+	};
+}
 function download_track(link) {
-	let stream = ytdl(link, { quality: "highestaudio" });
-	stream.on("progress", onProgress);
-	stream.on("info", (info) => {
-		//document.getElementById('progressBar').classList.add('visible')
-		document.getElementById("text-out").innerHTML +=
-			"Téléchargement de : " + info.videoDetails.title;
-		let start = Date.now();
+	const video = ytdl(link);
+	let title;
+	ytdl.getInfo(link).then((info) => {
+		console.log(info);
+		// On récupère la dernière valeur du tableau des thumbnails, last étant la meilleure qualité/resolution existante.
+		const thumbnail = info.videoDetails.thumbnails.last().url;
+		title = info.videoDetails.title;
+		const viewCount = info.videoDetails.viewCount;
 
-		const searchOptions = {
-			term: `${info.videoDetails.title}`,
-			limit: 1,
-		};
-
-		itunesAPI.searchItunes(searchOptions).then((result) => {
-			var metadata;
-			if (result.resultCount == 0) {
-				metadata = [
-					`-metadata`,
-					`title=${info.videoDetails.title}  `,
-					`-metadata`,
-					`artist=${info.videoDetails.author.name}  `,
-				];
-			} else {
-				metadata = [
-					`-metadata`,
-					`title=${result.results[0].trackName}  `,
-					`-metadata`,
-					`artist=${result.results[0].artistName}  `,
-					`-metadata`,
-					`album=${result.results[0].collectionName}  `,
-					`-metadata`,
-					`date=${result.results[0].releaseDate}  `,
-					`-metadata`,
-					`genre=${result.results[0].kind}  `,
-				];
-			}
-
-			var fichier_out = `${chemin}/${info.videoDetails.title.replace(
-				/\//g,
-				"-"
-			)}.${format}`;
-			if (process.platform == "win32")
-				fichier_out = `${chemin}/${info.videoDetails.title
-					.replace(/\//g, "-")
-					.replace(/[\<\>\:\«\|\?\*\.]*/g, "")}.${format}`;
-
-			ffmpeg(stream)
-				.audioBitrate(128)
-				.outputOptions(metadata)
-				.save(fichier_out)
-				.on("error", function (err, stdout, stderr) {
-					afficher_err(info.videoDetails.title, err);
-				})
-				.on("end", () => {
-					afficher_notif(info.videoDetails.title, taille + "MB");
-				});
+		$(".video-thumbnail").css({ "background-image": `url("${thumbnail}")` });
+		$(".video-title").text(title);
+		// $(".viewCount").text(viewCount + " vues");
+		$(".video-found").addClass("grid");
+		video.pipe(fs.createWriteStream(`${chemin}/${title}.${format}`));
+	});
+	video.on("progress", (chunkLength, downloaded, total) => {
+		const percent = downloaded / total;
+		$(".checkmark__circle").css({
+			"stroke-dashoffset": (percent * 166).toFixed(2) - 166,
 		});
+	});
+	video.on("end", () => {
+		$(".checkmark__check").fadeIn();
+		$(".checkmark").addClass("fill");
+		// $(".checkmark").fadeIn().delay(2500).fadeOut();
+		// toastr.success(`${title} a été téléchargé`);
 	});
 }
 
@@ -166,48 +148,52 @@ function dl_track_from_playlist(playlist, element) {
 	const videoItems = playlist.items;
 	const videoLength = playlist.items.length;
 
-	myFunc();
+	// myFunc();
 
-	async function getLinks() {
-		for (let i = 0; i < videoLength; i++) {
-			const videoUrl = videoItems;
-			return videoUrl;
-		}
-	}
+	// async function getLinks() {
+	// 	for (let i = 0; i < videoLength; i++) {
+	// 		const videoUrl = videoItems;
+	// 		return videoUrl;
+	// 	}
+	// }
 
-	async function myFunc() {
-		const links = await getLinks();
-		for (let link of links) {
-			let soundsUrl = link.shortUrl;
-			const stream = ytdl(soundsUrl, { quality: "highestaudio" });
-			stream.pipe(
-				fs.createWriteStream(
-					`${chemin}/${link.title.replace(/\//g, "-")}.${format}`
-				)
-			);
+	// async function myFunc() {
+	// 	const links = await getLinks();
+	// 	for (let link of links) {
+	// 		let soundsUrl = link.shortUrl;
+	// 		const stream = ytdl(soundsUrl, {
+	// 			quality: "highestaudio",
+	// 		});
+	// 		stream.pipe(
+	// 			fs.createWriteStream(
+	// 				`${chemin}/${link.title.replace(/\//g, "-")}.${format}`
+	// 			)
+	// 		);
 
-			stream.on("finish", () => {
-				toastr.success(`Finished ${link.title}`, { timeOut: 500 });
-				console.log(`${link.title}`);
-			});
-		}
-		// for (const link of links) {
-		// 	// const stream = ytdl(link.url, { quality: "highestaudio" });
-		// 	// stream.pipe(
-		// 	// 	fs.createWriteStream(
-		// 	// 		`${chemin}/${link.title.replace(/\//g, "-")}.${format}`
-		// 	// 	)
-		// 	// );
-		// 	// stream.on("progress", () => {
-		// 	// 	toastr.info(`Downloading ${link.title}`, { timeOut: 150 });
-		// 	// 	console.log(`${link.title}`);
-		// 	// });
-		// 	// stream.on("finish", () => {
-		// 	// 	toastr.success(`Finished ${link.title}`, { timeOut: 500 });
-		// 	// 	console.log(`${link.title}`);
-		// 	// });
-		// }
-	}
+	// 		stream.on("finish", () => {
+	// 			toastr.success(`Finished ${link.title}`, {
+	// 				timeOut: 500,
+	// 			});
+	// 			console.log(`${link.title}`);
+	// 		});
+	// 	}
+	// 	// for (const link of links) {
+	// 	// 	// const stream = ytdl(link.url, { quality: "highestaudio" });
+	// 	// 	// stream.pipe(
+	// 	// 	// 	fs.createWriteStream(
+	// 	// 	// 		`${chemin}/${link.title.replace(/\//g, "-")}.${format}`
+	// 	// 	// 	)
+	// 	// 	// );
+	// 	// 	// stream.on("progress", () => {
+	// 	// 	// 	toastr.info(`Downloading ${link.title}`, { timeOut: 150 });
+	// 	// 	// 	console.log(`${link.title}`);
+	// 	// 	// });
+	// 	// 	// stream.on("finish", () => {
+	// 	// 	// 	toastr.success(`Finished ${link.title}`, { timeOut: 500 });
+	// 	// 	// 	console.log(`${link.title}`);
+	// 	// 	// });
+	// 	// }
+	// }
 	// async function myFunc() {
 	// 	for (let i = 0; i < videoLength; i++) {
 	// 		const videoUrl = videoItems[i].url;
@@ -314,5 +300,7 @@ function save_param() {
 	fs.writeFileSync(appDir + "/userSetting.json", JSON.stringify(setting));
 }
 $("#saveParam").click(function () {
-	toastr.success(`Sauvegardé`, { timeOut: 100 });
+	toastr.success(`Sauvegardé`, {
+		timeOut: 100,
+	});
 });
